afbe30a0bea19cd33322787db18b62ceb21160d9
Michael C Klavuhn <michaelklavuhn3@gmail.com>
2021-06-18 17:45:31 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,7 +18,19 @@ class Transfer
     end
   end
     
-  
+  def execute_transaction
+    if @sender.balance < @amount
+      @status = "rejected"
+      return "Transaction rejected. Please check your account balance."
+
+    elsif @status == "complete"
+      puts "Transaction was already excuted"
+    else
+      @sender.deposit( @amount * -1 ) 
+      @receiver.deposit( @amount )
+      @status = "complete"
+    end
+  end
   
   
   
