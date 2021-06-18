333d76830c78e43e1813da3458a2614448f644d0
Michael C Klavuhn <michaelklavuhn3@gmail.com>
2021-06-18 18:44:10 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -29,7 +29,13 @@ class Transfer
     end
   end
   
-  
+  def reverse_transfer
+    if @status == "complete"
+      @sender.balance += @amount
+      @receiver.balance -= @amount
+      @status = "reversed"
+    end
+  end
   
   
   
