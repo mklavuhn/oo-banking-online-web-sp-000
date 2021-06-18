a51f973d470fcfc516225e1a8209b850114c180f
Michael C Klavuhn <michaelklavuhn3@gmail.com>
2021-06-18 18:27:19 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -24,7 +24,7 @@ class Transfer
       p "Transaction rejected. Please check your account balance."
 
     elsif status == "complete"
-      puts "Transaction was already excuted"
+      p "Transaction was already excuted"
     else
       @sender.deposit( amount * -1 ) 
       @receiver.deposit( amount )
