8780c17c45cbe70452f0c3bd52ef64949a598c51
Michael C Klavuhn <michaelklavuhn3@gmail.com>
2021-06-18 17:48:57 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -20,15 +20,15 @@ class Transfer
     
   def execute_transaction
     if sender.balance < amount
-      @status = "rejected"
+      status = "rejected"
       return "Transaction rejected. Please check your account balance."
 
     elsif @status == "complete"
       puts "Transaction was already excuted"
     else
-      sender.deposit( @amount * -1 ) 
-      @receiver.deposit( @amount )
-      @status = "complete"
+      sender.deposit( amount * -1 ) 
+      receiver.deposit( amount )
+      status = "complete"
     end
   end
   
