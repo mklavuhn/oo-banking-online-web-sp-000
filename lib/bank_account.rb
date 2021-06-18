c9b123f055bc499e0f4760ceb578074d9b290845
Michael C Klavuhn <michaelklavuhn3@gmail.com>
2021-06-18 17:23:54 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,7 +1,7 @@
 class BankAccount
 
-  attr_reader :name, :status
-  attr_accessor :balance
+  attr_reader :name
+  attr_accessor :balance, :status
   
 
   def initialize(name, balance = 1000, status = "open")
